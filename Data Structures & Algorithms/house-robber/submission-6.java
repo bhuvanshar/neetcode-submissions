94d9a852ea0f80b52fb39d7dae474feed9b8ee28
Bhuvan Sharma <iambhuvan0987@Gmail.com>
2026-05-28 00:04:39 +0530

    Add: house-robber - submission-6

---
+++ Data Structures & Algorithms/house-robber/submission-6.java
@@ -0,0 +1,42 @@
+class Solution {
+    public int rob(int[] nums) {
+        int [] dp = new int [nums.length];
+        Arrays.fill(dp, -1);
+        // return recursive(nums);
+        return tabulation(nums);
+    }
+    public int recursive(int [] nums){
+        int n = nums.length;
+        return solve(nums, n-1);
+    }
+    public int solve(int [] nums, int n){
+        if(n < 0)return 0;
+        if(n == 0)return nums[0];
+
+        return Math.max(solve(nums, n-1), solve(nums, n-2) + nums[n]);
+    }
+
+    public int mem(int [] nums, int i, int [] dp){
+        if(i == 0)return nums[0];
+        if(i < 0)return 0;
+        if(dp[i] != -1)return dp[i];
+        dp[i] = Math.max(mem(nums, i-1, dp), mem(nums, i-2, dp) + nums[i]);
+        return dp[i];
+    }
+    public int tabulation(int [] nums){
+        int n = nums.length;
+        int [] dp = new int [nums.length];
+        // Arrays.fill(dp, -1);
+if(n == 1)return nums[0];
+        dp[0] = nums[0];
+        dp[1] = Math.max(nums[0], nums[1]);
+
+        
+
+        for(int i=2;i<nums.length;i++){
+            dp[i] = Math.max(dp[i-1], dp[i-2] + nums[i]);
+        }
+        return dp[n-1];
+        
+    }
+}
