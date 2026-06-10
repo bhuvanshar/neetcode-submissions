a183fe164390473e3e89b80269e30c35ca2d73ed
Bhuvan Sharma <iambhuvan0987@Gmail.com>
2026-06-10 06:41:32 -0700

    Add: climbing-stairs - submission-17

---
+++ Data Structures & Algorithms/climbing-stairs/submission-17.java
@@ -0,0 +1,29 @@
+class Solution {
+    static int [] memo = new int [46];
+    public int climbStairs(int n) {
+        if(n <=2)return n;
+        Arrays.fill(memo, -1);
+        int [] dp = new int [n];
+
+        return solveDp(n, dp);
+    }
+    public int solve(int n){
+        if(n <= 1){
+            return 1;
+        }
+        if(memo[n] != -1){
+            return memo[n];
+        }
+
+     memo[n] = solve(n-1) + solve(n-2);
+     return memo[n];
+    }
+    public int solveDp(int n, int [] dp){
+       dp[0] = 1;
+       dp[1] = 2;
+       for(int i=2;i<n;i++){
+        dp[i] = dp[i-1] + dp[i-2];
+       }
+       return dp[n-1];
+    }
+}
