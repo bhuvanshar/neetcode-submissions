cd1c0e196d7b309746b234369cebd92b13bbfd98
Bhuvan Sharma <iambhuvan0987@Gmail.com>
2026-06-12 13:10:17 -0700

    Add: word-break - submission-1

---
+++ Data Structures & Algorithms/word-break/submission-1.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean wordBreak(String s, List<String> wordDict) {
+        Boolean [] memo = new Boolean [s.length()];
+        return solve(s, wordDict, 0, memo);
+    }
+    public boolean solve(String s, List<String> dict, int i, Boolean [] memo){
+        if(i == s.length())return true;
+        if(memo[i] != null)return memo[i];
+        for(String word : dict){
+            int len = word.length();
+            if(i + len <= s.length() && s.substring(i, i+len).equals(word)){
+                if(solve(s, dict, i+len, memo)){
+                    return memo[i] = true;
+                }
+            }
+        }
+        return memo[i] = false;
+    }
+}
