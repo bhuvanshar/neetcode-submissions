71d6373a32c7fc18fab4fab88d462025aa840bd2
Bhuvan Sharma <iambhuvan0987@Gmail.com>
2026-06-14 23:13:43 -0700

    Add: generate-parentheses - submission-1

---
+++ Data Structures & Algorithms/generate-parentheses/submission-1.java
@@ -0,0 +1,27 @@
+class Solution {
+    public List<String> generateParenthesis(int n) {
+        List<String> res= new ArrayList<>();
+        StringBuilder temp = new StringBuilder();
+        int open = 0, close = 0;
+        solve(open, close, temp, res, n);
+        return res;
+    }
+
+    public void solve(int open, int close,  StringBuilder temp, List<String> res, int n){
+        if(open == n && close == n && open == close){
+            res.add(new String(temp));
+            return;
+        }
+
+        if(open < n){
+            temp.append('(');
+            solve(open+1,close, temp, res, n);
+            temp.deleteCharAt(temp.length()-1);
+        }
+        if(close < open){
+            temp.append(")");
+            solve(open, close+1, temp, res, n);
+            temp.deleteCharAt(temp.length()-1);
+        }
+    }
+}
