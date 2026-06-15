e17031024e3a2854fab46a06cfc6edfceaca5983
Bhuvan Sharma <iambhuvan0987@Gmail.com>
2026-06-15 03:26:06 -0700

    Add: house-robber - submission-19

---
+++ Data Structures & Algorithms/house-robber/submission-19.java
@@ -0,0 +1,48 @@
+class Solution {
+    public int rob(int[] nums) {
+        int [] memo = new int [nums.length+1];
+        Arrays.fill(memo, -1);
+        // return memo(nums, nums.length-1, memo);
+        return tab(nums);
+    }
+
+    public int solve(int [] n, int i){
+        if(i < 0)return 0;
+        if(i <= 1)return n[i];
+
+        return Math.max(solve(n, i-1), solve(n, i-2) + n[i]);
+    }
+    public int memo(int [] n, int i, int [] memo){
+        if(i < 0)return 0;
+        if(i == 0)return n[0];
+        if(memo[i] != -1)return memo[i];
+
+        return memo[i] = Math.max(memo(n, i-1,memo), memo(n, i-2, memo) + n[i]);
+    }
+    public int tab(int [] num){
+        if(num.length == 0)return 0;
+        if(num.length == 1)return num[0];
+        int n = num.length;
+        int [] dp = new int [n+1];
+        dp[0] = num[0];
+        dp[1] = Math.max(num[0], num[1]);
+
+        for(int i=2;i<n;i++){
+            dp[i] = Math.max(dp[i-1], dp[i-2] + num[i]);
+        }
+        return dp[n-1];
+    }
+    public int spaceOpt(int [] nums){
+        if(nums.length == 0)return 0;
+        if(nums.length == 1)return nums[0];
+        int n = nums.length;
+        int one = nums[0];
+        int two = Math.max(nums[0], nums[1]);
+        for(int i=2;i<n;i++){
+            int temp = one;
+            one = Math.max(one, two + nums[i]);
+            two = temp;
+        }
+        return one;
+    }
+}
