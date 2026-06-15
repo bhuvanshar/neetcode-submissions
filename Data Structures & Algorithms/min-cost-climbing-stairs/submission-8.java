78b2d9e37242f709772df3def5e225555a019e2c
Bhuvan Sharma <iambhuvan0987@Gmail.com>
2026-06-15 02:01:34 -0700

    Add: min-cost-climbing-stairs - submission-8

---
+++ Data Structures & Algorithms/min-cost-climbing-stairs/submission-8.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int minCostClimbingStairs(int[] cost) {
+        int n = cost.length;
+        int [] memo = new int [n+1];
+        Arrays.fill(memo,-1);
+        return Math.min(memo(cost, n-1, memo), memo(cost, n-2,memo));
+        // return Math.min(solve(cost, n-1), solve(cost, n-2));
+    }
+    public int solve(int [] cost, int i){
+        if(i < 0)return 0;
+        if(i <= 1)return cost[i];
+
+        return Math.min(solve(cost, i-1), solve(cost, i-2)) + cost[i];
+    }
+
+    public int memo(int [] cost, int i, int [] memo){
+        if(i < 0)return 0;
+        if(i <= 1)return cost[i];
+        if(memo[i] != -1)return memo[i];
+        return memo[i] = Math.min(memo(cost, i-1, memo), memo(cost, i-2, memo))+cost[i];
+    }
+}
