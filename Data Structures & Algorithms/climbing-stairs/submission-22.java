4765685f7c2d180ebadbc27be7eee9dcb77f82eb
Bhuvan Sharma <iambhuvan0987@Gmail.com>
2026-06-15 01:10:26 -0700

    Add: climbing-stairs - submission-22

---
+++ Data Structures & Algorithms/climbing-stairs/submission-22.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int climbStairs(int n) {
+        int [] dp = new int [n+1];
+        Arrays.fill(dp, -1);
+        return tab(n);
+    }
+    public int solve(int n){
+        if(n <= 1)return 1;
+
+        return solve(n-1) + solve(n-2);
+    }
+
+    public int memo(int n, int [] dp){
+        if(n <= 1){
+            return 1;
+        }
+        if(dp[n] != -1)return dp[n];
+
+        return dp[n] = memo(n-1, dp) + memo(n-2, dp);
+    }
+
+    public int tab(int n){
+        int [] dp = new int [n+1];
+        dp[0] = 1;
+        dp[1] = 1;
+        for(int i=2;i<=n;i++){
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+        return dp[n];
+    }
+}
