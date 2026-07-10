97086518c859b4153a24e3ef709803d51d4d418a
Bhuvan Sharma <iambhuvan0987@Gmail.com>
2026-07-10 03:08:08 -0700

    Add: longest-palindrome - submission-1

---
+++ Data Structures & Algorithms/longest-palindrome/submission-1.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int longestPalindrome(String s) {
+        Map<Character, Integer> count = new HashMap<>();
+        int res = 0;
+
+        for (char c : s.toCharArray()) {
+            count.put(c, count.getOrDefault(c, 0) + 1);
+            if (count.get(c) % 2 == 0) {
+                res += 2;
+            }
+        }
+
+        for (int cnt : count.values()) {
+            if (cnt % 2 == 1) {
+                res += 1;
+                break;
+            }
+        }
+
+        return res;
+    }
+}
