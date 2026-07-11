cf9359d3a099a9414c8515d6b8a3c7df27abda04
Bhuvan Sharma <iambhuvan0987@Gmail.com>
2026-07-11 20:47:33 +0530

    Add: logger-rate-limiter - submission-3

---
+++ Data Structures & Algorithms/logger-rate-limiter/submission-3.java
@@ -0,0 +1,26 @@
+class Logger {
+    Map<String, Integer> map;
+    public Logger() {
+        this.map = new HashMap<>();
+    }
+    
+    public boolean shouldPrintMessage(int timestamp, String message) {
+        // int time = timestamp%10;
+        if(map.containsKey(message)){
+            if(timestamp - map.get(message) >= 10){
+                map.put(message, timestamp);
+                return true;
+            }else
+            return false;
+        }else{
+            map.put(message, timestamp);
+        }
+        return true;
+    }
+}
+
+/**
+ * Your Logger object will be instantiated and called as such:
+ * Logger obj = new Logger();
+ * boolean param_1 = obj.shouldPrintMessage(timestamp,message);
+ */
