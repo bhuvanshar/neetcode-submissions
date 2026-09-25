7ed90100bc8de86d77d6fa99d4599b6a3f923f84
Bhuvan Sharma <iambhuvan0987@Gmail.com>
2026-09-25 12:08:12 +0530

    Add: is-anagram - submission-29

---
+++ Data Structures & Algorithms/is-anagram/submission-29.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if(s.length() != t.length())return false;
+        int [] arr = new int [26];
+        for(int i=0;i<s.length();i++){
+            arr[(int)(s.charAt(i)-'a')]++;
+            arr[(int)(t.charAt(i) - 'a')]--;
+        }
+
+        for(int n : arr){
+            if(n != 0)return false;
+        }
+        return true;
+    }
+}
